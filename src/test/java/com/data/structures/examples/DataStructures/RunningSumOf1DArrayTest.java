f19cb3f14704a63294be7e6569f07c134be248cb
AchuthanKumar <achu2761998@gmail.com>
2025-05-01 16:46:10 +0530

    Running Sum of 1D Array

---
+++ src/test/java/com/data/structures/examples/DataStructures/RunningSumOf1DArrayTest.java
@@ -0,0 +1,34 @@
+package com.data.structures.examples.DataStructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RunningSumOf1DArrayTest {
+
+    @Autowired
+    private RunningSumOf1DArray runningSumOf1DArray;
+
+    @Test
+    public void testRunningSumOf1DArray() {
+        System.out.println(Arrays.toString(runningSumOf1DArray.runningSum(new int[]{1, 2, 3, 4})));
+        assertEquals(Arrays.toString(new int[]{1, 3, 6, 10}), Arrays.toString(runningSumOf1DArray.runningSum(new int[]{1, 2, 3, 4})));
+    }
+
+    @Test
+    public void testRunningSumOf1DArray1() {
+        System.out.println(Arrays.toString(runningSumOf1DArray.runningSum(new int[]{1, 1, 1, 1, 1})));
+        assertEquals(Arrays.toString(new int[]{1,2,3,4,5}), Arrays.toString(runningSumOf1DArray.runningSum(new int[]{1, 1, 1, 1, 1})));
+    }
+
+    @Test
+    public void testRunningSumOf1DArray2() {
+        System.out.println(Arrays.toString(runningSumOf1DArray.runningSum(new int[]{3, 1, 2, 10, 1})));
+        assertEquals(Arrays.toString(new int[]{3,4,6,16,17}), Arrays.toString(runningSumOf1DArray.runningSum(new int[]{3, 1, 2, 10, 1})));
+    }
+}
