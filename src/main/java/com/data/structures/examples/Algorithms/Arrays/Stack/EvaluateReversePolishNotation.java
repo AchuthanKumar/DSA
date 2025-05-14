7fadc906b032295d483856affddf0a9f0635e00c
AchuthanKumar <achu2761998@gmail.com>
2025-05-14 13:03:18 +0530

    Evaluate Reverse Polish Notation

---
+++ src/main/java/com/data/structures/examples/Algorithms/Arrays/Stack/EvaluateReversePolishNotation.java
@@ -0,0 +1,46 @@
+package com.data.structures.examples.Algorithms.Arrays.Stack;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Stack;
+
+@Service
+public class EvaluateReversePolishNotation {
+    Stack<Integer> operands;
+    int operand1;
+    int operand2;
+
+    public int evalRPN(String[] tokens) {
+        operands = new Stack<>();
+
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    operand2 = operands.pop();
+                    operand1 = operands.pop();
+                    operands.push(operand1 + operand2);
+                    break;
+                case "-":
+                    operand2 = operands.pop();
+                    operand1 = operands.pop();
+                    operands.push(operand1 - operand2);
+                    break;
+                case "*":
+                    operand2 = operands.pop();
+                    operand1 = operands.pop();
+                    operands.push(operand1 * operand2);
+                    break;
+                case "/":
+                    operand2 = operands.pop();
+                    operand1 = operands.pop();
+                    operands.push(operand1 / operand2);
+                    break;
+                default:
+                    operands.push(Integer.parseInt(token));
+                    break;
+            }
+        }
+
+        return operands.pop();
+    }
+}
---
+++ src/test/java/com/data/structures/examples/Algorithms/Stack/EvaluateReversePolishNotationTest.java
@@ -0,0 +1,29 @@
+package com.data.structures.examples.Algorithms.Stack;
+
+import com.data.structures.examples.Algorithms.Arrays.Stack.EvaluateReversePolishNotation;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class EvaluateReversePolishNotationTest {
+    @Autowired
+    EvaluateReversePolishNotation evaluateReversePolishNotation;
+
+    @Test
+    public void testEvaluateReversePolishNotation() {
+        assertEquals(9, evaluateReversePolishNotation.evalRPN(new String[]{"2", "1", "+", "3", "*"}));
+    }
+
+    @Test
+    public void testEvaluateReversePolishNotation1() {
+        assertEquals(6, evaluateReversePolishNotation.evalRPN(new String[]{"4","13","5","/","+"}));
+    }
+
+    @Test
+    public void testEvaluateReversePolishNotation2() {
+        assertEquals(22, evaluateReversePolishNotation.evalRPN(new String[]{"10","6","9","3","+","-11","*","/","*","17","+","5","+"}));
+    }
+}
