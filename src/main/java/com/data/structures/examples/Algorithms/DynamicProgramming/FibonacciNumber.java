16e27b89b057c23d7388421797f1c9b431f8bf8d
AchuthanKumar <achu2761998@gmail.com>
2025-07-03 11:21:46 +0530

    DP : Top-Down and Bottom Up approach

---
+++ src/main/java/com/data/structures/examples/Algorithms/DynamicProgramming/FibonacciNumber.java
@@ -0,0 +1,43 @@
+package com.data.structures.examples.Algorithms.DynamicProgramming;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FibonacciNumber {
+    //    DP  : Top-down approach without Memoization
+    public int fib(int n) {
+        if (n == 0 || n == 1) return n;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    //    Top-down approach with Memoization
+    public int fibWithMemoization(int n) {
+        int[] fibonacciSum = new int[n + 1];
+
+        return fibHelper(n, fibonacciSum);
+    }
+
+    private int fibHelper(int n, int[] fibonacciSum) {
+        if (n == 0 || n == 1) return n;
+
+        if (fibonacciSum[n] != 0) return fibonacciSum[n];
+
+        return fibonacciSum[n] = fibHelper(n - 1, fibonacciSum) + fibHelper(n - 2, fibonacciSum);
+    }
+
+    //    Bottom-Up Approach with Tabulation
+    public int fibBottomUpWithTabulation(int n) {
+        int[] fibonacciSum = new int[n + 1];
+
+        fibonacciSum[0] = 0;
+        fibonacciSum[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibonacciSum[i] = fibonacciSum[i - 1] + fibonacciSum[i - 2];
+        }
+
+        return fibonacciSum[n];
+    }
+
+}
---
+++ src/test/java/com/data/structures/examples/Algorithms/DynamicProgramming/FibonacciNumberTest.java
@@ -0,0 +1,58 @@
+package com.data.structures.examples.Algorithms.DynamicProgramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+public class FibonacciNumberTest {
+    @Autowired
+    FibonacciNumber fibonacciNumber;
+
+    @Test
+    public void testFibonacciNumber(){
+        assertEquals(1,fibonacciNumber.fib(2));
+    }
+
+    @Test
+    public void testFibonacciNumber1(){
+        assertEquals(2,fibonacciNumber.fib(3));
+    }
+
+    @Test
+    public void testFibonacciNumber2(){
+        assertEquals(3,fibonacciNumber.fib(4));
+    }
+
+    @Test
+    public void testFibonacciNumberWithMemoization(){
+        assertEquals(1,fibonacciNumber.fibWithMemoization(2));
+    }
+
+    @Test
+    public void testFibonacciNumberWithMemoization1(){
+        assertEquals(2,fibonacciNumber.fibWithMemoization(3));
+    }
+
+    @Test
+    public void testFibonacciNumberWithMemoization2(){
+        assertEquals(3,fibonacciNumber.fibWithMemoization(4));
+    }
+
+    @Test
+    public void testFibonacciNumberBottomUpWithTabulation(){
+        assertEquals(1,fibonacciNumber.fibBottomUpWithTabulation(2));
+    }
+
+    @Test
+    public void testFibonacciNumberBottomUpWithTabulation1(){
+        assertEquals(2,fibonacciNumber.fibBottomUpWithTabulation(3));
+    }
+
+    @Test
+    public void testFibonacciNumberBottomUpWithTabulation2(){
+        assertEquals(3,fibonacciNumber.fibBottomUpWithTabulation(4));
+    }
+}
