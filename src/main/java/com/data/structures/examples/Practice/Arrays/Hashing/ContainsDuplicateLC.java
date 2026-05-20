d14e7953a5fafa972bab411379668d0abd5910d8
AchuthanKumar <achu2761998@gmail.com>
2026-05-20 09:10:34 +0530

    Practice 2026 - arrays

--- src/main/java/com/data/structures/examples/Practice/Arrays/Hashing/ContainsDuplicateLC.java
+++ src/main/java/com/data/structures/examples/Practice/Arrays/Hashing/ContainsDuplicateLC.java
@@ -3,20 +3,17 @@ package com.data.structures.examples.Practice.Arrays.Hashing;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class ContainsDuplicateLC {
 
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> distinctSet = new HashSet<>();
-
+        Set<Integer> distinctSet = new HashSet<>();
         for (int num : nums) {
-            if (distinctSet.contains(num))
-                return true;
-
+            if (distinctSet.contains(num)) return true;
             distinctSet.add(num);
         }
-
         return false;
     }
 }
